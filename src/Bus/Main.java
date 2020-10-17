87e34382acc0a2d9282ed7ec8fc491e921777376
=Tree <phtom1999@gmail.com>
2020-10-17 17:12:49 +0700

    Initial commit

---
+++ src/Bus/Main.java
@@ -0,0 +1,12 @@
+package Bus;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestDijkstraAlgorithm test = new TestDijkstraAlgorithm();
+		test.testExcute();
+
+	}
+
+}
